7e69ff87f3ec4f04a0e49e0de4387b8c96cc380c
suhanik016 <suhanik092@gmail.com>
2024-11-03 18:05:47 +0530

    sastashadi.com

--- index.js
+++ index.js
@@ -1,72 +1,56 @@
-// function Sum(a,b){
-//     return a+b;
-// }
-// function sub(x,y){
-//     return x-y;
-// }
-// function multiply(x,y){
-//     return x*y;
-// }
-// console.log(Sum(11,9));
-// console.log(sub(11,9));
-// console.log(multiply(11,9));
-// function Sum(a,b){
-//     return a+b;
-// }
-// console.log(Sum(10,5));
-// function Sum(a,b){
-//     return a+b;
-// }
-// var a = 10;
-// var b = 15;
-// console.log(Sum(a,b));
-// const sum = (a,b)=>{
-//     return a+b;
-// };
-// console.log(sum(10,20));
-function submit2() {
-    var husbandDay = document.getElementById("tap3").value;
-    var husbandMonth = document.getElementById("tap4").value;
-    var husbandYear = document.getElementById("tap5").value;
+var express = require("express");
+var app = express(); // Corrected this line
+var bodyParser = require("body-parser");
+var path = require("path");
 
-    console.log(husbandDay, husbandMonth, husbandYear);
-}
 
-function submit3() {
-    var wifeDay = document.getElementById("tap6").value;
-    var wifeMonth = document.getElementById("tap7").value;
-    var wifeYear = document.getElementById("tap8").value;
+app.use(express.static(path.join(__dirname,'public')))
 
-    console.log(wifeDay, wifeMonth, wifeYear);
-}
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-function finalSubmit() {
-    var husbandDay = parseInt(document.getElementById("tap3").value);
-    var husbandMonth = parseInt(document.getElementById("tap4").value);
-    var husbandYear = parseInt(document.getElementById("tap5").value);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    var wifeDay = parseInt(document.getElementById("tap6").value);
-    var wifeMonth = parseInt(document.getElementById("tap7").value);
-    var wifeYear = parseInt(document.getElementById("tap8").value);
 
+
+app.post("/", (req, res) => {
+    console.log(req.body.husbandname);
+    console.log(req.body.wifename);
+    console.log(req.body.husbandday);
+    console.log(req.body.wifeday);
+    console.log(req.body.husbandmonth);
+    console.log(req.body.wifemonth);
+    console.log(req.body.husbandyear);
+    console.log(req.body.wifeyear);
+   
     var c = 0;
 
-    if (husbandDay % 2 === 0 && wifeDay % 2 === 0) {
-        c = 1;
-    }
-    if ((husbandMonth % 2 === 0 && wifeMonth % 2 !== 0) || (husbandMonth % 2 !== 0 && wifeMonth % 2 === 0)) {
-        c = 2;
-    }
-    if ( wifeYear - 4 === husbandYear ||wifeYear - 5 === husbandYear) {
-        c = 3;
-    }
+    if (req.body.husbandday % 2 === 0 && req.body.wifeday % 2 === 0 || req.body.husbandday % 2 != 0 && req.body.wifeday % 2 != 0) {
+        c++;
+    } 
 
-    console.log(c);
-    if (c === 3) {
-        alert("accept shadi");
+    if ((req.body.husbandmonth % 2 == 0 && req.body.wifemonth % 2 != 0) || (req.body.husbandmonth % 2 != 0 && req.body.wifemonth % 2== 0)) {
+        c++;
+        
+    } 
+    if ((req.body.wifeyear +5 == req.body.husbandyear)){
+        c++;
+    }
+    if(c==3){
+        res.send("shaadi karlo")
+        
     }
     else{
-        alert("dusra dekho")
+        c=0;
+        res.send("shadi mat karo")
     }
-}
 
+    
+
+
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
